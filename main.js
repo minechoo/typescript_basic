796d8296a6d05a699690db2285ee404cff0cebe1
minechoo <choominhae@gmail.com>
2023-09-05 11:48:25 +0900

    인터페이스를 활용해서 공통된 구조의 함수 타입지정

--- main.js
+++ main.js
@@ -1,30 +1,17 @@
-//기본 타입
-let test = 'hello';
-let num = 0;
-let isFemale = true;
-//참조형
-let like = ['game', 'music', 'movie'];
-let dislike = ['study', 'homework', 'exercise'];
-let odd = [1, 3, 5];
-let even = [2, 4, 6];
-let abc = [true, false];
-let bbc = [true, false];
-//tuple : 배열에 들어가는 자료형이 다를때 배열의 순서마다 타입지정
-let arr1 = [3, '3'];
-//Student라는 커스텀 인터페이스 타입이 지정된 객체
-let student1 = {
-    name: 'David',
-    age: 20,
-    isFemale: false,
-    //address: 'Seoul',
-    1: 'C',
+//함수의 return값에 타입을 지전하면 비효울적인 이유
+//1. 이미 리턴된 값이기 때문에 타입오류가 뜨더라도 할 수 있는 작업이 없음
+//2. 에러자체가 호출단에서 발생하는게 아닌 함수정의단에서 리턴되는 시접에 발생하기때문에 호출시 오류파악이 힘듬
+const plus = (n1, n2) => {
+    return n1 + n2;
 };
-student1.name = 'andy';
-let student2 = {
-    name: 'Emily',
-    age: 30,
-    isFemale: true,
-    address: 'Busan',
-    2: 'B',
+const minus = (n1, n2) => {
+    return n1 - n2;
 };
-student2[2] = 'A';
+const multiply = (n1, n2) => {
+    return n1 * n2;
+};
+const divide = (n1, n2) => {
+    return n1 / n2;
+};
+const result = plus(3, 4);
+console.log(result);
--- main.ts
+++ main.ts
@@ -1,45 +1,27 @@
-//기본 타입
-let test: string = 'hello';
-let num: number = 0;
-let isFemale: boolean = true;
+//함수의 return값에 타입을 지전하면 비효울적인 이유
+//1. 이미 리턴된 값이기 때문에 타입오류가 뜨더라도 할 수 있는 작업이 없음
+//2. 에러자체가 호출단에서 발생하는게 아닌 함수정의단에서 리턴되는 시접에 발생하기때문에 호출시 오류파악이 힘듬
 
-//참조형
-let like: string[] = ['game', 'music', 'movie'];
-let dislike: Array<string> = ['study', 'homework', 'exercise'];
-let odd: number[] = [1, 3, 5];
-let even: Array<number> = [2, 4, 6];
-let abc: boolean[] = [true, false];
-let bbc: Array<boolean> = [true, false];
-//tuple : 배열에 들어가는 자료형이 다를때 배열의 순서마다 타입지정
-let arr1: [number, string] = [3, '3'];
+//공통된 함수의 구조는 동일한 인터페이스 적용가능
 
-type Score = 'A' | 'B' | 'C' | 'D' | 'E';
-
-//객체 타입지정
-//interface : 객체처럼 자료의 구조가 복잡한 경우 직접 커스터마이징된 타입을 생성
-interface Student {
-	name: string;
-	age: number;
-	readonly isFemale: boolean; //해당 property는 읽기 전용, 변경 불가능
-	address?: string; //해당 타입의 property는 optional 하게 적용
-	[grade: number]: Score; //property 값을 산정할 수 없을때 대괄호로 묶어서 타입지정 가능
+interface Calc {
+	(n1: number, n2: number): any;
 }
-//Student라는 커스텀 인터페이스 타입이 지정된 객체
-let student1: Student = {
-	name: 'David',
-	age: 20,
-	isFemale: false,
-	//address: 'Seoul',
-	1: 'C',
+const plus: Calc = (n1, n2) => {
+	return n1 + n2;
 };
 
-student1.name = 'andy';
-
-let student2: Student = {
-	name: 'Emily',
-	age: 30,
-	isFemale: true,
-	address: 'Busan',
-	2: 'B',
+const minus: Calc = (n1, n2) => {
+	return n1 - n2;
 };
-student2[2] = 'A';
+
+const multiply: Calc = (n1, n2) => {
+	return n1 * n2;
+};
+
+const divide: Calc = (n1, n2) => {
+	return n1 / n2;
+};
+
+const result: number = plus(3, 4);
+console.log(result);
