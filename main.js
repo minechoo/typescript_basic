5d69da3b8a3fb5905244423651270578c23298cc
minechoo <choominhae@gmail.com>
2023-09-05 10:24:56 +0900

    first

---
+++ main.js
@@ -0,0 +1,5 @@
+const plus = (n1, n2) => {
+    return n1 + n2;
+};
+const result = plus(2, 3);
+console.log(result);
---
+++ main.ts
@@ -0,0 +1,6 @@
+const plus = (n1: number, n2: number) => {
+	return n1 + n2;
+};
+
+const result = plus(2, 3);
+console.log(result);
